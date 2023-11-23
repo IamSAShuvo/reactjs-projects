d8ffe779129e662d2d4fba238a57e554e3716b79
IamSAShuvo <salmanazizshuvo@gmail.com>
2023-11-24 00:18:04 +0600

    3(rgb) buttons added to change background color

--- basicBgChanger/src/App.jsx
+++ basicBgChanger/src/App.jsx
@@ -1,13 +1,37 @@
-// import { useState } from "react";
-import "./App.css";
+import { useState } from "react";
+// import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
-      <h1 className="bg-red-700">I am Ready to Hack</h1>
-    </>
+    <div className="w-full h-screen duration-200" style={{ background: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-3 px-2 bg-orange-500">
+        <div className="flex flex-wrap justify-center gap-3 rounded-3xl shadow-lg bg-white px-3 py-2 ">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
